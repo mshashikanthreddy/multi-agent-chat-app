592d19657a868d1d21f6f641ca01fb9ae709f6b7
mshashikanthreddy <shashikanthreddymulinti@gmail.com>
2026-06-03 15:51:51 +0530

    some changes dns

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    const dns = require('node:dns/promises');
-    await dns.setServers(['1.1.1.1', '8.8.8.8']);
-    await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.PASSWORD}@backend-projects.ewiwyeo.mongodb.net/multi-agent-chat-app`);
-    console.log("✅ MongoDB Connected Successfully");
-  } catch (error) {
-    console.error("❌ MongoDB Connection Error:", error.message);
-    process.exit(1);
-  }
-};
 
+
+const connectDB = async () =>{
+  try{
+    const dns = require('node:dns/promises')
+    await dns.setServers(['1.1.1.1', '8.8.8.8'])
+    await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.PASSWORD}@backend-projects.ewiwyeo.mongodb.net/multi-agent-chat-app`)
+    console.log("✅ MongoDB Connected Successfully")
+  }catch (error) {
+    console.error("❌ MongoDB Connection Error:", error.message)
+    process.exit(1)}
+}
 
 module.exports =  connectDB;
 
